8621906ad9b2980a6b642296106852203d26cb72
Artem <agaleev583@gmail.com>
2024-11-25 16:26:10 +0300

    feat:finish

--- script.js
+++ script.js
@@ -4,11 +4,24 @@ const taskList = document.querySelector(".task-list");
 
 function addNewtask(event) {
     event.preventDefault();
+    let task = taskInput.value;
     
-    console.log(event);
-    
-    
+    if (!task) return;
+    const newItem = document.querySelector("#template").cloneNode(true).content; 
+    newItem.querySelector(".task__text").value = task;
+
+    newItem.querySelector(".task__delete").addEventListener("click", deleteTask)
+
+    taskList.append(newItem);
+    taskInput.value = "";
+    taskInput.focus();
+
+
+}
+
+function deleteTask(event) {
+    const target = event.target.parentElement;
+    target.remove()
 }
 
 addBtn.addEventListener("click", addNewtask)
-
